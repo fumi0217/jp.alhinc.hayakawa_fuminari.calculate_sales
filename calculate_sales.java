3bb09c11140691723d2df3f6d542b9239e868097
fuminari <bwb90382@gmail.com>
2017-08-02 05:56:15 +0900

    adding error msg

--- calculate_sales.java
+++ calculate_sales.java
@@ -4,27 +4,36 @@ class calculate_sales{
     public static void main(String args[]){
         File file = new File(args[0]);
         File file2 = new File(args[1]);
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(file));
 
-            String str = null;
+        if(!file.exists()){
+            System.out.println("支店定義ファイルが存在しません");
+            System.exit(0);
+        }else if(!file2.exists()){
+            System.out.println("商品定義ファイルが存在しません");
+            System.exit(0);
+        }else{
+            try{
+                BufferedReader br = new BufferedReader(new FileReader(file));
 
-            while((str=br.readLine())!=null){
-                System.out.println(str);
+                String str = null;
+
+                while((str=br.readLine())!=null){
+                    System.out.println(str);
+                }
+
+                BufferedReader br2 = new BufferedReader(new FileReader(file2));
+                while((str=br2.readLine())!=null){
+                    System.out.println(str);
+                }
+
+                br.close();
+            }catch(FileNotFoundException e){
+                System.out.println(e);
+            }catch(IOException e){
+                System.out.println(e);
             }
 
-            BufferedReader br2 = new BufferedReader(new FileReader(file2));
-            while((str=br2.readLine())!=null){
-                System.out.println(str);
-            }
-
-            br.close();
-        }catch(FileNotFoundException e){
-            System.out.println(e);
-        }catch(IOException e){
-            System.out.println(e);
+            System.out.println("ダウンロード完了");
         }
-
-        System.out.println("ダウンロード完了");
     }
 }
