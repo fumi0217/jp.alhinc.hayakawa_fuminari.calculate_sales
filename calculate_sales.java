fb7b830fdcc044d5184d094c186d7f9e89b138a5
fuminari <bwb90382@gmail.com>
2017-08-02 05:05:47 +0900

    make it while repetion

--- calculate_sales.java
+++ calculate_sales.java
@@ -3,31 +3,29 @@ import java.io.*;
 class calculate_sales{
     public static void main(String args[]){
 
+        int i = 0;
+
         while(args[i]!=null){
+            File file = new File(args[i]);
+            try{
+                BufferedReader br = new BufferedReader(new FileReader(file));
 
-        }
-        File file = new File(args[0]);
-        File file2 = new File(args[1]);
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(file));
+                String str = null;
 
-            String str = null;
-
-            while((str=br.readLine())!=null){
-                System.out.println(str);
+                while((str=br.readLine())!=null){
+                    System.out.println(str);
+                }
+                br.close();
+            }catch(FileNotFoundException e){
+                System.out.println(e);
+            }catch(IOException e){
+                System.out.println(e);
             }
 
-            BufferedReader br2 = new BufferedReader(new FileReader(file2));
-            while((str=br2.readLine())!=null){
-                System.out.println(str);
-            }
+            i++;
 
-            br.close();
-        }catch(FileNotFoundException e){
-            System.out.println(e);
-        }catch(IOException e){
-            System.out.println(e);
         }
+
 
         System.out.println("ダウンロード完了");
     }
