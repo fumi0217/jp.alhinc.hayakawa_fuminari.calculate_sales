1b0aaf42e475c7f7b879c541f42a12e107baa80a
fuminari <bwb90382@gmail.com>
2017-08-10 11:15:14 +0900

    adding comments

--- calculate_sales.java
+++ calculate_sales.java
@@ -6,9 +6,11 @@ import java.util.regex.Matcher;
 
 class calculate_sales{
     public static void main(String args[]){
+        //accepting command lines
         File file = new File(args[0]);
         File file2 = new File(args[1]);
 
+        //adding error messages if files cannot be found in the URL provided
         if(!file.exists()){
             System.out.println("支店定義ファイルが存在しません");
             System.exit(0);
@@ -21,18 +23,22 @@ class calculate_sales{
 
                 String str = null;
 
+                    //creating HashMap to store data about branch and commodity
                     Map<Integer,String> branch = new HashMap<Integer,String>();
                     Map<String,String> commodity = new HashMap<String,String>();
 
+                //putting what we get from the first command line into branch HashMap
                 while((str=br.readLine())!=null){
                     String[] strAry = str.split(",");
                     Integer num = Integer.parseInt(strAry[0]);
                     branch.put(num,strAry[1]);
 
+                    //adding error messages if the format is unnapropriate
                     String regex = "\\d\\d\\d";
 
                     Pattern p = Pattern.compile(regex);
                     Matcher m = p.matcher(strAry[0]);
+
 
                     if(!m.find()){
                         System.out.println("支店定義ファイルのフォーマットが不正です");
@@ -41,15 +47,18 @@ class calculate_sales{
                 }
                 System.out.println("支店定義ファイルを読み込みました");
 
+                //printing out what is inside branch HashMap
                 for(Map.Entry<Integer,String> e : branch.entrySet()){
                     System.out.println(e.getKey()+":"+e.getValue());
                 }
 
+                //putting what we get from the second command line into commodity HashMap
                 br = new BufferedReader(new FileReader(file2));
                 while((str=br.readLine())!=null){
                     String[] strAry = str.split(",");
                     commodity.put(strAry[0],strAry[1]);
 
+                    //adding error messages if the format is unnapropriate
                     String regex = "[0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z]";
 
                     Pattern p = Pattern.compile(regex);
@@ -62,6 +71,7 @@ class calculate_sales{
                 }
                 System.out.println("商品定義ファイルを読み込みました");
 
+                //printing out what is inside branch HashMap
                 for(Map.Entry<String,String> e : commodity.entrySet()){
                     System.out.println(e.getKey()+":"+e.getValue());
                 }
